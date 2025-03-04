e73d62b05be934aef049f2be4ccc267e33c4e9b1
“joanomana” <joansebastianomasu@gmail.com>
2025-03-03 21:35:47 -0500

    feat: :lipstick: Adding style for login page

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,15 +1,17 @@
-import { Geist, Geist_Mono } from "next/font/google";
+
+import { Montserrat } from "next/font/google";
+
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+const montserrat = Montserrat({
+  variable: "--font-montserrat", 
+  subsets: ["latin"], 
+  weight: ["400", "500", "600","700"], 
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+
+
+
 
 export const metadata = {
   title: "Create Next App",
@@ -19,9 +21,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={montserrat.variable}>
         {children}
       </body>
     </html>
